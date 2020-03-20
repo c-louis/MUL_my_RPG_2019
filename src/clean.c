968dd0b623d8d8841a0b1e7508128ab8bca78163
Mathias Ducatillon <mathias5.ducatillon@epitech.eu>
2020-03-20 16:08:20 +0100

    [+] Init base files

---
+++ src/clean.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** my_rpg
+** File description:
+** clean allocated resources
+*/
+
+#include "rpg.h"
+
+void clean_memory(globals_t *gl)
+{
+    return;
+}
---
+++ src/loop.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2020
+** my_rpg
+** File description:
+** Draw loop
+*/
+
+#include "rpg.h"
+
+void main_loop(sfRenderWindow *window, globals_t *gl)
+{
+    sfRenderWindow_clear(window, sfBlack);
+    sfRenderWindow_display(window);
+}
